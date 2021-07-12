576c6235646cdb12621768eb6cd2b48e29d45f5e
lucasmetron <lucasmetron@gmail.com>
2021-07-12 15:07:58 -0300

    incializando rotas

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import Home from './components/Home';
 import Aula from './components/Aulas';
 import Sobre from './components/Sobre';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//BrowserRouter é responsavel por gerenciar as rotas (as é para apelidar o BrouserRouter)
+//Route é onde iremos setar a rota
+
 function App() {
   return (
-    <div className="app" >
-      <Nav></Nav>
-      <Home></Home>
-      <Aula></Aula>
-      <Sobre></Sobre>
-    </div>
+
+    <Router>
+      <div className="app" >
+
+        <Nav></Nav>
+        <Route path='/'> {/* Setando a rota onde deve ser exibido o componente */}
+          <Home></Home>
+        </Route>
+        <Route path='/aulas'> {/* Setando a rota onde deve ser exibido o componente */}
+          <Aula></Aula>
+        </Route>
+        <Route path='/sobre'> {/* Setando a rota onde deve ser exibido o componente */}
+          <Sobre></Sobre>
+        </Route>
+
+      </div>
+    </Router>
+
   );
 }
 
