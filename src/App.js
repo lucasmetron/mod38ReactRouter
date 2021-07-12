69c9ca0317762dc53b2c43f4e5692ba86ba582d8
lucasmetron <lucasmetron@gmail.com>
2021-07-12 15:32:01 -0300

    implementado o switch

--- src/App.js
+++ src/App.js
@@ -4,28 +4,38 @@ import Home from './components/Home';
 import Aula from './components/Aulas';
 import Sobre from './components/Sobre';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //BrowserRouter é responsavel por gerenciar as rotas (as é para apelidar o BrouserRouter)
 //Route é onde iremos setar a rota
+//Switch vai acessar a primeira rota que der match e nao vai tentar outras
 
 function App() {
   return (
 
     <Router>
       <div className="app" >
-
         <Nav></Nav>
-        <Route path='/'> {/* Setando a rota onde deve ser exibido o componente */}
-          <Home></Home>
-        </Route>
-        <Route path='/aulas'> {/* Setando a rota onde deve ser exibido o componente */}
-          <Aula></Aula>
-        </Route>
-        <Route path='/sobre'> {/* Setando a rota onde deve ser exibido o componente */}
-          <Sobre></Sobre>
-        </Route>
 
+        <Switch> {/*Componente para fazer o switch das rotas*/}
+
+          <Route exact path='/'> {/* passado o exact para acessar a rota exata*/}
+            <Home></Home>
+          </Route>
+          <Route exact path='/aulas'> {/* passado o exact para acessar a rota exata*/}
+            <Aula></Aula>
+          </Route>
+          <Route exact path='/sobre'> {/* passado o exact para acessar a rota exata*/}
+            <Sobre></Sobre>
+          </Route>
+          <Route exact path='/aulas/a'>
+            <div>
+              Coutedo
+            </div>
+          </Route>
+
+        </Switch>
       </div>
+
     </Router>
 
   );
