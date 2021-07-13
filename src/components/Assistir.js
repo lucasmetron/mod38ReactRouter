9581d59abb59900bffcdf66b8e94808ef27e80c8
lucasmetron <lucasmetron@gmail.com>
2021-07-13 15:19:37 -0300

    use location

---
+++ src/components/Assistir.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useLocation, useParams, useRouteMatch } from 'react-router-dom';
+
+
+export default function Assistir(props) {
+
+    const query = useLocation().search.slice(1);
+
+    useEffect(() => {
+        console.log(query)
+        console.log(match)
+
+    })
+
+    return (
+        <div className="center">
+            <iframe width="727" height="409" src={`https://www.youtube.com/embed/${query}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    )
+}
